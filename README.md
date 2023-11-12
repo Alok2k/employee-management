2f2bd72e93ab7c92689c2ca2c026e2f2b14dc512
Alok2k <krishnabhatt1010@gmail.com>
2023-11-12 11:21:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# employee-management
+ In this project, we have designed REST APIs for all CRUD operations that can be used for Employee Management, using Spring Boot features and saving the data in MySQL using Spring Data JPA.
---
+++ employee-management-system-backend/src/test/java/com/alok/employee/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
